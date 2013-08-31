880ed54a4ea4e326fcfa3ddf9b6b94321c959812
Anthony Levings <sketchytech@gylphi.co.uk>
2013-08-31 14:50:06 +0100

    added hyperlinking to notes

--- php/indexing_in_progress/chapter-parser.php
+++ php/indexing_in_progress/chapter-parser.php
@@ -4,31 +4,33 @@ include_once('readjson.php');
 global $itemNumber;
 $itemNumber=1;
 $linkedItemNumber=6;
-global $notes;
-$notes=array();
+global $para;
+$para=new paragraph; // create new paragraph object from 
 
 
 // Chapter
 $chapter_json = file_get_contents("burn.json");
 $chapter = json_decode($chapter_json);
 
-processObject($chapter,$itemNumber,$linkedItemNumber);
-printNotes();  // After chapter has been processed, notes are printed at end
+processObject($chapter,$linkedItemNumber);
+// After chapter has been processed create note list and print
+global $notes; // makes notes globally available to all functions
+$notes=$para->returnNotes(); // create the array of notes by importing the one created in readjson.php
+printNotes();  // notes are printed
 
 
-function processObject($chapter,$itemNumber,$linkedItemNumber) {
+function processObject($chapter,$linkedItemNumber) {
 global $itemNumber;
 
 if(is_object($chapter)){
 
 foreach($chapter as $key=>$value)
 {
-if($itemNumber==$linkedItemNumber) echo "<a id='".$itemNumber."'></a>";
+// if($itemNumber==$linkedItemNumber) echo "<a id='".$itemNumber."'></a>";
 $title_tags = array("h1","h2","h3","h4","h5");
 if(in_array($key, $title_tags)){ 
 echo "<".$key.">".$value."</".$key.">";
 echo "<a id='".$itemNumber."'>ANCHORPOINT ".$itemNumber."</a>";
-//$itemNumber++;
 }
 if($key=="blockquote") {
 echo "<".$key.">";
@@ -36,46 +38,46 @@ echo "<".$key.">";
 }
 // Confirm that chapter is array
 if (is_array($value)) {
-
+global $itemNumber;
 $numberOfItems=count($value);
 $i=0;
 while ($i<$numberOfItems){
-// $itemNumber++;
+
 //arrayProcess($value);
 if(is_string($value[$i])) {
+
 echo $value[$i];
 }// Part of the error testing 
-elseif(is_array($value[$i])) arrayProcess($value[$i],$itemNumber,$linkedItemNumber);
-elseif(is_object($value[$i])) bounceObject($value[$i],$itemNumber,$linkedItemNumber);
+elseif(is_array($value[$i])) arrayProcess($value[$i],$linkedItemNumber);
+elseif(is_object($value[$i])) bounceObject($value[$i],$linkedItemNumber);
 
 $i++;
 }
 }
 if($key=="blockquote") {echo "</".$key.">"; // this handles blockquotes that come between paragraphs but blockquotes within paragraphs must also be handled, because those are correct
+
 echo "<a id='".$itemNumber."'>ANCHORPOINT ".$itemNumber."</a>";
-// $itemNumber++;
+
 }
 }
 }
 }
-function arrayProcess($array,$itemNumber,$linkedItemNumber){
+function arrayProcess($array,$linkedItemNumber){
 global $itemNumber;
 if(is_string($array[0])){
 global $notes;
-$para=new paragraph;
+global $para;
 $para->returnParagraph($array);
-$new_notes=$para->returnNotes();
-if($new_notes) $notes=array_merge($notes,$new_notes);
 }
 
-else processObject($array,$itemNumber,$linkedItemNumber);
+else processObject($array,$linkedItemNumber);
 echo "<a id='".$itemNumber."'>ANCHORPOINT ".$itemNumber."</a>";
 $itemNumber++;
 }
 
-function bounceObject($object,$itemNumber,$linkedItemNumber) {
+function bounceObject($object,$linkedItemNumber) {
 global $itemNumber;
-processObject($object,$itemNumber,$linkedItemNumber);
+processObject($object,$linkedItemNumber);
 $itemNumber++;
 }
 function printNotes(){
@@ -87,10 +89,12 @@ else echo "<h2>Notes</h2>";
 echo "<ol>";
 $i=0;
 while($i<count($notes)){
-echo "<li>".$notes[$i]."</li>";
+$note_number=$i+1;
+echo "<li id='note".$note_number."'>".$notes[$i]." <a href='#ref".$note_number."'>[^]</a></li>";
 $i++;
 }
-echo "</ol>";}
+echo "</ol>";
+}
 }
 
 
