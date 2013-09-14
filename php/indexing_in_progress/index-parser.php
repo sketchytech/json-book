23b6a59a31d8c6e8302a66d87abd6ee7380410d4
Anthony Levings <sketchytech@gylphi.co.uk>
2013-09-14 10:56:42 +0100

    Update index-parser.php
    Item numbers in Index parsing now match those in Chapter parsing

--- php/indexing_in_progress/index-parser.php
+++ php/indexing_in_progress/index-parser.php
@@ -1,20 +1,27 @@
 <?php
 
 include_once('readjson.php');
-
-
+global $searchTerm;
+if(isset($_GET['search_term'])){
+ $searchTerm=$_GET['search_term'];
+}
+else $searchTerm='what'; 
+global $itemNumber;
+$itemNumber=0;
 // This index search currently only searches for strings within paragraphs not objects
 // Chapter
 $chapter_json = file_get_contents("burn.json");
 $chapter = json_decode($chapter_json);
 
-processObject($chapter,0);
+processObject($chapter);
 
-function processObject($chapter,$itemNumber) {
-$itemNumber++; // increment item when an object is processed - this shouldn't include objects within paragraphs
+function processObject($chapter) {
+global $itemNumber;
+//$itemNumber++; // increment item when an object is processed - this shouldn't include objects within paragraphs
 if(is_object($chapter)){
 foreach($chapter as $key=>$value)
 {
+// the item number needs to be incremented with each paragraph, heading, blockquote but not character styles
 
 if (is_array($value)) {
 
@@ -24,15 +31,19 @@ while ($i<$numberOfItems){
 //arrayProcess($value);
 
 if(is_array($value[$i])){
-$itemNumber++; // increments number for each paragraph
-arrayProcess($value[$i],$itemNumber);
+//$itemNumber++; // increments number for each paragraph
+arrayProcess($value[$i]);
 }
-elseif(is_object($value[$i])) bounceObject($value[$i],$itemNumber);
+elseif(is_object($value[$i])) bounceObject($value[$i]);
 
 $i++;
 }
 }
-if($key=="blockquote") echo "</".$key.">"; // this handles blockquotes that come between paragraphs but blockquotes within paragraphs must also be handled, because those are correct
+if($key=="blockquote"&&is_array($value)) {
+ // include paragraphs within blockquotes
+//$itemNumber=$itemNumber+count($value); // NOT NECESSARY
+}
+
 
 
 }
@@ -40,23 +51,35 @@ if($key=="blockquote") echo "</".$key.">"; // this handles blockquotes that come
 }
 
 }
-function arrayProcess($array,$itemNumber){
+function arrayProcess($array){
 $i=0;
-
+global $itemNumber;
+// Add to paragraph number for each array processed - we need to include headings but not styled text as well	
+$itemNumber++;
 while ($i<count($array)) {
-
+global $searchTerm;
 if(is_string($array[$i])){
-if(preg_match('/(what)/i',$array[$i])) {
+
+if(preg_match('/('.$searchTerm.')/i',$array[$i])) {
 // Replace fixed search term with $_GET from URL
 $para=new paragraph;
-$para->returnParagraph($array,'/(what)/i');
+$para->returnParagraph($array,'/('.$searchTerm.')/i');
 echo "Item ".$itemNumber; // this will be replaced by a hyperlink to chapter parser with itemNumber in query string, which will create an anchor point at the paragraph and take reader straight there
+$i=count($array); // Once the word has been found once in the sentence, force the end of the search for this paragraph 
 }
 }
 else if(is_object($array[$i])){
+
 // Send to object to function with array to search styled string
 //searchStyledString($array[$i],$array,$itemNumber);
-echo "<b>OBJECT IN PARA</B>";
+$basic_tags = array("h1","h2","h3","h4","h5","b","i","sup","sub","blockquote"); // Finds text with basic styles - should be extended for linked styles
+$key=key($array[$i]);
+if(in_array($key,$basic_tags)&&preg_match('/('.$searchTerm.')/i',$array[$i]->{$key})){
+
+$para=new paragraph;
+$para->returnParagraph($array,'/('.$searchTerm.')/i');
+$i=count($array); // Once the word has been found once in the sentence, force the end of the search for this paragraph 
+}
 
 }
 //else processObject($array,$itemNumber); // check sense of this
@@ -65,13 +88,17 @@ $i++;
 }
 }
 
-function bounceObject($object,$itemNumber) {
-processObject($object,$itemNumber);
+function bounceObject($object) {
+global $itemNumber;
+processObject($object);
+$itemNumber++;
 }
 
-function searchStyledString ($object,$paragraph,$itemNumber){
+function searchStyledString ($object,$paragraph){
+
 foreach($object as $key=>$value)
 {
+
 /*if(is_string($value)) {
 if(preg_match('/(what)/i',$value)) {
 // Replace fixed search term with $_GET from URL
@@ -84,4 +111,4 @@ echo $value;
 }
 }
 
-	?>
+?>
