d584e06927a92f00676491ae20c3893b29e82f79
Anthony Levings <sketchytech@gylphi.co.uk>
2015-04-11 22:11:11 +0100

    Update Typesetter.swift

--- iOS/Sources/Typesetter.swift
+++ iOS/Sources/Typesetter.swift
@@ -1,8 +1,13 @@
-struct Typesetter {
+class Typesetter:NSObject, NSLayoutManagerDelegate {
     let textStorage:NSTextStorage
     // an instance is created with an NSAttributedString
     init (attrStr:NSAttributedString) {
         textStorage = NSTextStorage(attributedString: attrStr)
+    }
+    
+    func layoutManager(layoutManager: NSLayoutManager, didCompleteLayoutForTextContainer textContainer: NSTextContainer?, atEnd layoutFinishedFlag: Bool) {
+        println("called")
+
     }
     // if views are for a (paging) scroll view then text views will given x position that places them one after the other
     func textViews(frame:CGRect, prepareForScrollView:Bool = true)->[UITextView] {
@@ -11,32 +16,34 @@ struct Typesetter {
         // create the layout manager
         let layoutManager = NSLayoutManager()
         // have an inset to be on the safe side
-        let inset:CGFloat = 10
+        let inset:CGFloat = 20
         
         // add the layout manager to the textStorage
         textStorage.addLayoutManager(layoutManager)
 
         // calculate the number of required views
-        let numberOfViews = Int(ceil(textStorage.boundingRectWithSize(CGSize(width: frame.width, height: frame.height-inset), options: .UsesDeviceMetrics | .UsesFontLeading | .UsesLineFragmentOrigin, context: nil).height / frame.height))
+        let numberOfViews = Int(ceil(textStorage.boundingRectWithSize(CGSize(width: frame.width, height: frame.height-inset), options: .UsesFontLeading | .UsesLineFragmentOrigin, context: nil).height / (frame.height-inset)))
         // see http://stackoverflow.com/questions/14409897/how-to-calculate-the-height-of-an-nsattributedstring-with-given-width-in-ios-6
         // there will sometimes be a blank final page but better than missing text, might be a better delegate method that could be used but didn't find one to judge when all text in textStorage has been placed into views
         
         // time to create the required views
         for i in 0...numberOfViews {
-            // create text container, make slightly smaller so it's inset from the provided frame size, same as the UITextView
+            // create text container, make slightly smaller so it's inset from the provided frame size
             let textContainer = NSTextContainer(size: CGSize(width: frame.width, height: frame.height-inset))
             // add text container to layout manager
             layoutManager.addTextContainer(textContainer)
             // set x-position
             let xposition:CGFloat = prepareForScrollView == true ? frame.width*CGFloat(i) : 0
             // create text view
-            let textView = UITextView(frame: CGRect(x: xposition, y: 0, width: frame.width, height: frame.height-inset), textContainer: layoutManager.textContainers[i] as? NSTextContainer)
+            let textView = UITextView(frame: CGRect(x: xposition, y: 0, width: frame.width, height: frame.height), textContainer: layoutManager.textContainers[i] as? NSTextContainer)
             // prevent crashing when attempt to scroll UITextView inside UIScroll view by disabling interaction
             textView.userInteractionEnabled = false
             // add view to array
             textViews.append(textView)
+            
         }
         // return the array
+        println(textViews.last?.text.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
         return textViews
     }
 }
